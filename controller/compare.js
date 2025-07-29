f04097c0cbeda539ea95d7f76659efc2e147c7ba
ShreyasN <shreyasnaik81@gmail.com>
2025-07-29 11:30:27 +0530

    new design

--- controller/compare.js
+++ controller/compare.js
@@ -1,3 +1,10 @@
+const {analyzerfetch}=require("./apifetch")
+
 const compare=(req,res)=>{
-    
+    const user1=req.body.user1;
+    const user2=req.body.user2;
+
+    if(user2==none){
+        analyzerfetch
+    }
 }
